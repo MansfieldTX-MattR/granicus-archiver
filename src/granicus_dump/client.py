25c270f362b4726a3e91703013e15496b847195b
MansfieldTX-MattR <matt.reid@mansfieldtexas.gov>
2024-07-23 12:44:30 -0500

    Use separate schedulers for download, copy and general

--- src/granicus_dump/client.py
+++ src/granicus_dump/client.py
@@ -1,4 +1,4 @@
-from typing import TypeVar, Generic, Coroutine, Self, Any, TYPE_CHECKING
+from typing import TypeVar, TypedDict, Literal
 import asyncio
 from pathlib import Path
 import dataclasses
@@ -14,6 +14,7 @@ from yarl import URL
 
 from .parser import parse_page
 from .model import ClipCollection, Clip, ParseClipData, ParseClipLinks, ClipFileKey
+from .utils import JobWaiters
 
 DATA_URL = URL('https://mansfieldtx.granicus.com/ViewPublisher.php?view_id=6')
 
@@ -31,16 +32,33 @@ class DownloadError(Exception): ...
 
 #     # async def __aenter__(self) -> Self:
 
-SCHEDULER: aiojobs.Scheduler|None = None
-def get_scheduler(limit: int|None = None) -> aiojobs.Scheduler:
-    global SCHEDULER
-    if SCHEDULER is None:
+class SchedulersTD(TypedDict):
+    general: aiojobs.Scheduler
+    downloads: aiojobs.Scheduler
+    copies: aiojobs.Scheduler
+
+SchedulerKey = Literal['general', 'downloads', 'copies']
+
+
+SCHEDULERS: SchedulersTD|None = None
+def get_schedulers(limit: int|None = None) -> SchedulersTD:
+    global SCHEDULERS
+    if SCHEDULERS is None:
         if limit is None:
-            raise RuntimeError('scheduler limit must be set')
-        SCHEDULER = aiojobs.Scheduler(limit=limit)
+            raise RuntimeError('scheduler limits must be set')
+        SCHEDULERS = {
+            'general':aiojobs.Scheduler(limit=32),
+            'downloads':aiojobs.Scheduler(limit=limit),
+            'copies':aiojobs.Scheduler(limit=limit),
+        }
     elif limit is not None:
-        raise RuntimeError('scheduler already created')
-    return SCHEDULER
+        raise RuntimeError('schedulers already created')
+    return SCHEDULERS
+
+
+def get_scheduler(key: SchedulerKey, limit: int|None = None) -> aiojobs.Scheduler:
+    d = get_schedulers(limit=limit)
+    return d[key]
 
 
 async def get_main_data(session: ClientSession, base_dir: Path) -> ClipCollection:
@@ -90,7 +108,7 @@ async def replace_pdf_links(
 
 
 async def replace_all_pdf_links(session: ClientSession, clips: ClipCollection) -> None:
-    scheduler = get_scheduler()
+    scheduler = get_scheduler('general')
     jobs: set[aiojobs.Job[ParseClipData]] = set()
     for clip in clips:
         if clip.parse_data.actual_links is not None:
@@ -102,14 +120,15 @@ async def replace_all_pdf_links(session: ClientSession, clips: ClipCollection) -
 
 
 async def download_clip(session: ClientSession, clip: Clip):
-    scheduler = get_scheduler()
+    download_waiter = JobWaiters(scheduler=get_scheduler('downloads'))
+    copy_waiter = JobWaiters(scheduler=get_scheduler('copies'))
 
     async def download_file(
         key: ClipFileKey,
         url: URL,
         filename: Path,
         temp_dir: Path
-    ) -> asyncio.Task:
+    ) -> aiojobs.Job:
         temp_filename = temp_dir / filename.name
         logger.debug(f'download {url} to {filename}')
         chunk_size = 64*1024
@@ -129,8 +148,8 @@ async def download_clip(session: ClientSession, clip: Clip):
             if stat.st_size != meta.content_length:
                 raise DownloadError(f'Filesize mismatch: {clip.unique_name=}, {key=}, {url=}, {stat.st_size=}, {response.headers=}')
         logger.debug(f'download complete for "{clip.unique_name} - {key}"')
-        copy_task = asyncio.create_task(copy_clip_to_dest(key, src_file=temp_filename, dst_file=filename))
-        return copy_task
+        copy_coro = copy_clip_to_dest(key, src_file=temp_filename, dst_file=filename)
+        return await copy_waiter.spawn(copy_coro)
 
 
     async def copy_clip_to_dest(key: ClipFileKey, src_file: Path, dst_file: Path) -> None:
@@ -148,28 +167,16 @@ async def download_clip(session: ClientSession, clip: Clip):
     with tempfile.TemporaryDirectory() as temp_dir:
         temp_dir = Path(temp_dir).resolve()
         clip.root_dir_abs.mkdir(exist_ok=True, parents=True)
-        download_jobs: set[aiojobs.Job[asyncio.Task]] = set()
-        download_job_waiters: set[asyncio.Task[asyncio.Task]] = set()
-        copy_tasks: set[asyncio.Task] = set()
         for key, url, filename in clip.iter_url_paths():
             if filename.exists():
                 logger.debug(f'filename exists: {key=}, {filename=}')
                 continue
-            logger.debug(f'{scheduler.active_count=}')
-            job = await scheduler.spawn(download_file(key, url, filename, temp_dir))
-            download_jobs.add(job)
-            download_job_waiters.add(asyncio.create_task(job.wait()))
-        while len(download_job_waiters):
-            done, pending = await asyncio.wait(download_job_waiters, return_when=asyncio.FIRST_COMPLETED)
-            for t in done:
-                exc = t.exception()
-                if exc is not None:
-                    raise exc
-                copy_task = t.result()
-                copy_tasks.add(copy_task)
-                download_job_waiters.discard(t)
-        if len(copy_tasks):
-            await asyncio.gather(*copy_tasks)
+            # logger.debug(f'{scheduler.active_count=}')
+            coro = download_file(key, url, filename, temp_dir)
+            await download_waiter.spawn(coro)
+
+        await download_waiter.gather()
+        await copy_waiter.gather()
 
     logger.success(f'clip "{clip.unique_name}" complete')
 
@@ -201,7 +208,9 @@ async def amain(
     scheduler_limit: int,
     max_clips: int|None = None,
 ):
-    scheduler = get_scheduler(limit=scheduler_limit)
+    schedulers = get_schedulers(limit=scheduler_limit)
+    waiter = JobWaiters(scheduler=schedulers['general'])
+
     local_clips: ClipCollection|None = None
     if data_file.exists():
         local_clips = ClipCollection.load(data_file)
@@ -213,25 +222,23 @@ async def amain(
         clips.save(data_file)
         check_all_clip_files(clips)
         i = 0
-        jobs: set[aiojobs.Job] = set()
         for clip in clips:
             if max_clips == 0:
                 break
             if clip.complete:
                 logger.debug(f'skipping {clip.unique_name}')
                 continue
-            logger.debug(f'{scheduler.active_count=}')
-            job = await scheduler.spawn(download_clip(session, clip))
-            jobs.add(job)
+            # logger.debug(f'{scheduler.active_count=}')
+            await waiter.spawn(download_clip(session, clip))
             i += 1
             if max_clips is not None and i >= max_clips:
                 break
-        if len(jobs):
-            logger.debug(f'awaiting {len(jobs)} jobs')
-            await asyncio.gather(*[job.wait() for job in jobs])
-        logger.info('closing scheduler..')
-        await scheduler.close()
-        logger.debug('scheduler closed')
+        await waiter
+        logger.info('closing schedulers..')
+        scheduler_list: list[aiojobs.Scheduler] = [schedulers[key] for key in schedulers.keys()]
+        await asyncio.gather(*[sch.close() for sch in scheduler_list])
+
+        logger.debug('schedulers closed')
         clips.save(data_file)
     return clips
 
@@ -247,13 +254,13 @@ async def amain(
     required=False,
 )
 @click.option('--max-clips', type=int, required=False)
-@click.option('--job-limit', type=int, default=8, show_default=True)
+@click.option('--io-job-limit', type=int, default=8, show_default=True)
 @click.option('--check-only', is_flag=True)
 def main(
     out_dir: Path,
     data_file: Path|None,
     max_clips: int|None,
-    job_limit: int,
+    io_job_limit: int,
     check_only: bool,
 ):
     if data_file is None:
@@ -265,7 +272,7 @@ def main(
     clips = asyncio.run(amain(
         data_file=data_file,
         out_dir=out_dir,
-        scheduler_limit=job_limit,
+        scheduler_limit=io_job_limit,
         max_clips=max_clips,
     ))
 
