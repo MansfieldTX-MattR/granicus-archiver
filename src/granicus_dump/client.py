a2d3133dd7df28866e7d4c0c13b286d2687c326e
MansfieldTX-MattR <matt.reid@mansfieldtexas.gov>
2024-07-24 13:16:07 -0500

    Add cli command to correct datetimes from f374718d7

--- src/granicus_dump/client.py
+++ src/granicus_dump/client.py
@@ -277,6 +277,29 @@ def check(obj: BaseContext):
     clips = ClipCollection.load(obj.data_file)
     check_all_clip_files(clips)
 
+@cli.command
+@click.pass_obj
+def fix_dts(obj: BaseContext):
+    out_dir, data_file = obj.out_dir, obj.data_file
+
+    async def do_fix():
+        local_clips: ClipCollection|None = None
+        if data_file.exists():
+            local_clips = ClipCollection.load(data_file)
+        async with ClientSession() as session:
+            clips = await get_main_data(session, out_dir)
+        if local_clips is not None:
+            local_keys = set(local_clips.clips.keys())
+            overlap = set(clips.clips.keys()) & local_keys
+            clips = clips.merge(local_clips)
+            assert len(overlap)
+            for key in overlap:
+                local_clip = local_clips.clips[key]
+                clip = clips.clips[key]
+                assert local_clip.parse_data.datetime == clip.parse_data.datetime
+            clips.save(data_file)
+
+    asyncio.run(do_fix())
 
 @cli.command
 @click.option('--max-clips', type=int, required=False)
