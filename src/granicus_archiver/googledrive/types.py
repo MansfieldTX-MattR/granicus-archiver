9d7c2cfe14d32b75d9a41590071d01d57cc1a19d
MansfieldTX-MattR <matt.reid@mansfieldtexas.gov>
2024-09-27 13:44:24 -0500

    Add missing docstrings for types

--- src/granicus_archiver/googledrive/types.py
+++ src/granicus_archiver/googledrive/types.py
@@ -16,37 +16,64 @@ __all__ = (
 T = TypeVar('T')
 
 FileId = NewType('FileId', str)
+"""A unique id for a file or directory in Drive"""
 
 class FileMeta(TypedDict):
-    name: str
-    id: NotRequired[FileId]
-    mimeType: NotRequired[str]
-    parents: NotRequired[list[FileId]]
-    size: NotRequired[str]
-    webViewLink: NotRequired[str]
-    webContentLink: NotRequired[str]
+    """Metadata for a Drive file or folder
+    """
+    name: str                           #: The file or folder name
+    id: NotRequired[FileId]             #: The item id (not required for uploads)
+    mimeType: NotRequired[str]          #: Content type
+    parents: NotRequired[list[FileId]]  #: Parent folder(s)
+    size: NotRequired[str]              #: Size in bytes
+    webViewLink: NotRequired[str]       #: Sharable link to the item
+    webContentLink: NotRequired[str]    #: Download link
+
 
 class FileMetaFull(TypedDict):
-    name: str
-    id: FileId
-    mimeType: str
-    parents: list[FileId]
-    size: str
-    webViewLink: str
-    webContentLink: str
+    """Similar to :class:`FileMeta` but with all fields required
+    """
+    name: str                           #: The file or folder name
+    id: FileId                          #: The item id
+    mimeType: str                       #: Content type
+    parents: list[FileId]               #: Parent folder(s)
+    size: str                           #: Size in bytes
+    webViewLink: str                    #: Sharable link to the item
+    webContentLink: str                 #: Download link
+
 
 class FileUploadResponse(TypedDict):
-    id: FileId
+    """Response type for a Drive upload
+    """
+    id: FileId                          #: The file id
 
 class FilePageResponse(TypedDict, Generic[T]):
+    """A single result during pagination in :class:`FileListResponse`
+    """
     files: Iterable[T]
+    """Iterable of either :class:`FileMeta` or :class:`FileMetaFull` objects
+    """
+
 
 FileListResponse = AsyncGenerator[FilePageResponse[T], None]
+"""Response type for file lists as an :term:`asynchronous generator` yielding
+:class:`FilePageResponse` objects
+"""
+
 
 class DriveResource(GoogleAPI):
+    """Overload for :class:`aiogoogle.resource.GoogleAPI` as a Drive resource
+
+    This wraps the response of :meth:`aiogoogle.resource.GoogleAPI.discover`
+    with ``api_name`` and ``api_version`` arguments set to ``("drive", "v3")``
+    """
     files: DriveFiles
+    """A :class:`DriveFiles` resource
+    """
 
 class DriveFiles(Resource):
+    """Resource for Drive files
+    """
     def list(self, **kwargs) -> Request: ...
 
     def create(self, **kwargs) -> Request: ...
