bfb747414375887dd6424d3ce57b016f7e5f9b48
MansfieldTX-MattR <matt.reid@mansfieldtexas.gov>
2024-10-02 11:48:08 -0500

    Allow timezone to be set from string

--- src/granicus_archiver/__init__.py
+++ src/granicus_archiver/__init__.py
@@ -6,6 +6,8 @@ from .legistar.rss_parser import set_timezone as legistar_set_timezone
 
 
 
-def set_local_timezone(tz: ZoneInfo):
+def set_local_timezone(tz: ZoneInfo|str):
+    if not isinstance(tz, ZoneInfo):
+        tz = ZoneInfo(tz)
     model_set_timezone(tz)
     legistar_set_timezone(tz)
